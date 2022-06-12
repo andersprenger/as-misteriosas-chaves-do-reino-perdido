996f8ab4285ca743d1f1270b516437ea07d378a4
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2022-06-12 14:33:24 -0300

    func move

--- As misteriosas chaves do reino perdido.playground/Contents.swift
+++ As misteriosas chaves do reino perdido.playground/Contents.swift
@@ -2,25 +2,34 @@ import Foundation
 
 class LostKingdom {
     var matrix: [[Character]]
+    var list: [Node]
     var markedNodes: Set<Node>
+    var visitedNodes: Set<Node>
     var keys: Set<Character>
     var lockedDoors: Set<Door>
     var players: [Player]
+    var caso: String
     
     init(_ arquivoCaso: String = "caso00") {
         print(arquivoCaso)
         
         matrix = loadMatrix(from: arquivoCaso)
         markedNodes = []
+        visitedNodes = []
         keys = []
         lockedDoors = []
         players = []
+        caso = arquivoCaso
+        list = []
+
         
         searchForPlayers()
         
         for player in players {
             dfs(from: player)
         }
+        
+        dfs(from: Player("3", in: Node(22, 55)))
     }
     
     func searchForPlayers() {
@@ -40,74 +49,26 @@ class LostKingdom {
         }
         
 //        print("\n")
-////        print(players)
+//        print(players)
 //        print("\n")
     }
     
     func dfs(from player: Player) {
         markedNodes = []
-        var visitedNodes: Set<Node> = []
-        markedNodes.insert(player.node)
-        var list: [Node] = [player.node]
+        visitedNodes = []
         
+        list = [player.node]
+        
+        markedNodes.insert(player.node)
         visitedNodes.insert(player.node)
         
         while !list.isEmpty {
             let visited = list.removeFirst()
             
-            for x in visited.x - 1 ... visited.x + 1 where x >= 0 && x < matrix.count {
-                for y in visited.y - 1 ... visited.y + 1 where y >= 0 && y < matrix[x].count {
-                    let isMarked = markedNodes.contains(Node(x, y))
-                    
-                    guard Node(x, y) != visited && !isMarked else {
-                        continue
-                    }
-                    
-                    markedNodes.insert(Node(x, y))
-                    
-                    let char = matrix[x][y]
-                    
-                    switch char {
-                    case ".":
-                        list = [Node(x, y)] + list
-                        
-                        visitedNodes.insert(Node(x,y))
-                        
-                    case "1","2","3","4","5","6","7","8","9":
-                        list = [Node(x, y)] + list
-                        
-                        visitedNodes.insert(Node(x,y))
-                        
-                    case "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z":
-                        list = [Node(x, y)] + list
-                        
-                        visitedNodes.insert(Node(x,y))
-
-                        keys.insert(char)
-                        
-                        for door in lockedDoors where door.char == char.uppercased().first {
-                            list = [Node(x, y)] + list
-        
-                            lockedDoors.remove(door)
-                            markedNodes.remove(door.node)
-                            
-                            visitedNodes.insert(door.node)
-                        }
-                        
-                    case "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z":
-                        if keys.contains(char.lowercased().first!) {
-                            list = [Node(x, y)] + list
-                            
-                            visitedNodes.insert(Node(x,y))
-                        } else {
-                            lockedDoors.insert(Door(char, Node(x, y)))
-                        }
-                        
-                    default:
-                        break
-                    }
-                }
-            }
+            move(from: Node(visited.x - 1, visited.y), list: &list)
+            move(from: Node(visited.x + 1, visited.y), list: &list)
+            move(from: Node(visited.x, visited.y - 1), list: &list)
+            move(from: Node(visited.x, visited.y + 1), list: &list)
         }
         
 //        check()
@@ -115,15 +76,65 @@ class LostKingdom {
         print("Player \(player.char):", visitedNodes.count)
     }
     
-    func check() {
-        var checkMatrix: [[Character]] = loadMatrix(from: "caso00")
+    func move(from node: Node, list: inout [Node]) {
+        let isMarked = markedNodes.contains(node)
         
-        for node in markedNodes where checkMatrix[node.x][node.y] != "#" {
+        guard !isMarked else {
+            return
+        }
+        markedNodes.insert(node)
+        
+        let char = matrix[node.x][node.y]
+        
+        switch char {
+        case ".","1","2","3","4","5","6","7","8","9":
+            list = [node] + list
+            
+            visitedNodes.insert(node)
+            
+        case "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z":
+            list = [node] + list
+            
+            visitedNodes.insert(node)
+
+            keys.insert(char)
+            
+            for door in lockedDoors where door.char == char.uppercased().first {
+                list = [node] + list
+
+                lockedDoors.remove(door)
+                markedNodes.remove(door.node)
+                
+                visitedNodes.insert(door.node)
+            }
+            
+        case "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z":
+            if keys.contains(char.lowercased().first!) {
+                list = [node] + list
+                
+                visitedNodes.insert(node)
+            } else {
+                lockedDoors.insert(Door(char, node))
+            }
+            
+        default:
+            break
+        }
+    }
+    
+    func check() {
+        var checkMatrix: [[Character]] = loadMatrix(from: caso)
+        
+        for node in visitedNodes {
             checkMatrix[node.x][node.y] = "^"
         }
         
+        printMatrix(checkMatrix)
+    }
+    
+    func printMatrix(_ matrix: [[Character]]) {
         var str = ""
-        for line in checkMatrix {
+        for line in matrix {
             for char in line {
                 str += "\(char)"
             }
@@ -135,10 +146,10 @@ class LostKingdom {
     }
 }
 
-LostKingdom("caso00")
+//LostKingdom("caso00")
 LostKingdom("caso05")
-LostKingdom("caso06")
-LostKingdom("caso07")
-LostKingdom("caso08")
-LostKingdom("caso09")
-LostKingdom("caso10")
+//LostKingdom("caso06")
+//LostKingdom("caso07")
+//LostKingdom("caso08")
+//LostKingdom("caso09")
+//LostKingdom("caso10")
