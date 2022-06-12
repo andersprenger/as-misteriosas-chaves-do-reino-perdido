7b7065e5fce6e379698a031794420f9a2928362c
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2022-06-12 18:10:01 -0300

    removido visitedNodes, usando checkedNodes para calculo de nodos percorridos

--- As misteriosas chaves do reino perdido.playground/Sources/LostKingdom.swift
+++ As misteriosas chaves do reino perdido.playground/Sources/LostKingdom.swift
@@ -4,32 +4,30 @@ public class LostKingdom {
     var matrix: [[Character]]
     var list: [Node]
     var markedNodes: Set<Node>
-    var visitedNodes: Set<Node>
+    
     var keys: Set<Character>
     var lockedDoors: Set<Door>
     var players: [Player]
-    var caso: String
     
-    public init(_ arquivoCaso: String = "caso00") {
-        print(arquivoCaso)
+    var fileName: String
+    
+    public init(_ fileName: String = "caso00") {
+        print(fileName)
         
-        matrix = loadMatrix(from: arquivoCaso)
+        matrix = loadMatrix(from: fileName)
         markedNodes = []
-        visitedNodes = []
+        
         keys = []
         lockedDoors = []
         players = []
-        caso = arquivoCaso
+        self.fileName = fileName
         list = []
-        
         
         searchForPlayers()
         
         for player in players {
             dfs(from: player)
         }
-        
-        //        dfs(from: Player("3", in: Node(22, 55)))
     }
     
     func searchForPlayers() {
@@ -54,12 +52,10 @@ public class LostKingdom {
     
     func dfs(from player: Player) {
         markedNodes = []
-        visitedNodes = []
         
         list = [player.node]
         
         markedNodes.insert(player.node)
-        visitedNodes.insert(player.node)
         
         while !list.isEmpty {
             let visited = list.removeFirst()
@@ -70,9 +66,9 @@ public class LostKingdom {
             move(from: Node(visited.x, visited.y + 1))
         }
         
-        //        check()
+//        check()
         
-        print("Player \(player.char):", visitedNodes.count)
+        print(markedNodes.count)
     }
     
     func move(from node: Node) {
@@ -81,21 +77,30 @@ public class LostKingdom {
         guard !isMarked else {
             return
         }
-        markedNodes.insert(node)
+        
+        guard node.x >= 0 && node.x < matrix.count else {
+            return
+        }
+        
+        guard node.y >= 0 && node.y < matrix[node.x].count else {
+            return
+        }
         
         let char = matrix[node.x][node.y]
+
+        guard char != "#" else {
+            return
+        }
+        
+        markedNodes.insert(node)
+        
         
         switch char {
         case ".","1","2","3","4","5","6","7","8","9":
             list = [node] + list
             
-            visitedNodes.insert(node)
-            
         case "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z":
             list = [node] + list
-            
-            visitedNodes.insert(node)
-            
             keys.insert(char)
             
             for door in lockedDoors where door.char == char.uppercased().first {
@@ -103,15 +108,11 @@ public class LostKingdom {
                 
                 lockedDoors.remove(door)
                 markedNodes.remove(door.node)
-                
-                visitedNodes.insert(door.node)
             }
             
         case "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z":
             if keys.contains(char.lowercased().first!) {
                 list = [node] + list
-                
-                visitedNodes.insert(node)
             } else {
                 lockedDoors.insert(Door(char, node))
             }
@@ -122,9 +123,9 @@ public class LostKingdom {
     }
     
     func check() {
-        var checkMatrix: [[Character]] = loadMatrix(from: caso)
+        var checkMatrix: [[Character]] = loadMatrix(from: fileName)
         
-        for node in visitedNodes {
+        for node in markedNodes {
             checkMatrix[node.x][node.y] = "^"
         }
         
